dc6cf94c6b8176f909856642fa28802258eee919
Ed1son <ed1son@Ed1sondeMacBook-Pro.local>
2019-05-08 22:11:45 +0800

    -add change the port

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.get('/doc/:docName', function(req, res) {
   })
 })
  
-var server = app.listen(8000, function () {
+var server = app.listen(8001, function () {
  
   var host = server.address().address
   var port = server.address().port
