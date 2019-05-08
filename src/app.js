8199b30d8bd59752f40196021eac49b097f8089b
Ed1son <ed1son@Ed1sondeMacBook-Pro.local>
2019-05-08 21:54:45 +0800

    first comit

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+
+
+// 解析 application/json
+app.use(bodyParser.json()); 
+// 解析 application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded());
+ 
+app.post('/port', function (req, res) {
+  console.log(req.body);
+
+  let data = JSON.stringify({
+          port: 'JPSDe123'
+      })
+  res.send(data);
+})
+
+app.get('/doc/:docName', function(req, res) {
+  console.log(req.params.docName);
+  let docName = req.params.docName;
+  const docRootPath = path.resolve(__dirname, '..');
+  
+
+  fs.readFile(docRootPath + '/public/doc/' + docName, function(err, data) {
+    if(err) {
+      res.send("0");
+    } else {
+      console.log(data.toString());
+      let str = data.toString();
+      res.setHeader('Content-Type', 'text/html');
+      res.send(str);
+    }
+  })
+})
+ 
+var server = app.listen(8000, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("app running in http://%s:%s", host, port)
+ 
+})
